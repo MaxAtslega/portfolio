eb210fecdad10ac8e0bb957c44a6820468c90f85
MaxAtslega <max.atslega@hotmail.com>
2023-03-26 17:17:07 +0200

    Fix: Type error

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,13 +2,14 @@ import Nav from '@/components/shared/Nav'
 import { Container } from '@/components/shared/Container'
 import { Roboto } from 'next/font/google'
 import Footer from '@/components/shared/Footer'
+import { ReactNode } from 'react'
 
 const roboto = Roboto({
   weight: '400',
   subsets: ['latin'],
 })
 
-export default function Layout({ children }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className={roboto.className}>
       <Nav />
